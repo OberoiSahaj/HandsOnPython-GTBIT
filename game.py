73367295e4425eb77447ecc0d443986b4dfa6342
Sahaj Oberoi <oberoi_sahaj@yahoo.com>
2019-11-08 16:15:09 +0530

    comments added

--- game.py
+++ game.py
@@ -1,21 +1,23 @@
 import random
 
-options = ['stone','paper','scissors']
+options = ['stone','paper','scissors'] 
 
 user = None
 
-while True:
+while True: # For continuous loop
     
-    user = input('Enter Stone, Paper or Scissors:  ')
+    user = input('Enter Stone, Paper or Scissors:  ') #Input by user
     user = user.lower()
 
-    if user == 'exit':
+    if user == 'exit': # If user enters 'exit' then it will come out of loop.
         break
     
-    comp = random.choice(options)
+    comp = random.choice(options) #Computer chooses it's choice at random
 
-    if user not in options:
+    if user not in options: #if user doesn't enter the desired options
         user = input('Invalid option. Enter again: ')
+        
+    #Conditions if computer chooses Stone   
     if comp == 'stone' and user == 'paper':
         print('Computer chose Stone, so you Win')        
     elif comp == 'stone' and user == 'scissors':
@@ -23,6 +25,7 @@ while True:
     elif comp == 'stone' and user == 'stone':
         print('Computer chose Stone, so it is a Tie')
                     
+    #Conditions if computer chooses Paper
     if comp == 'paper' and user == 'paper':
         print('Computer chose Paper, so it is a Tie')
     elif comp == 'paper' and user == 'scissors':
@@ -30,6 +33,7 @@ while True:
     elif comp == 'paper' and user == 'stone':
         print('Computer chose Paper, so you Lose')
                     
+    #Conditions if computer chooses Scissors
     if comp == 'scissors' and user == 'scissors':
         print('Computer chose Scissors, so it is a Tie')
     elif comp == 'scissors' and user == 'stone':
