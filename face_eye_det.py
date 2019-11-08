a082b3d6750969e44a9b20e691d9352d08c9c519
Sahaj Oberoi <oberoi_sahaj@yahoo.com>
2019-11-09 03:04:17 +0530

    updated with comments

--- face_eye_det.py
+++ face_eye_det.py
@@ -1,46 +1,35 @@
-import cv2  
-  
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')  
-  
-# capture frames from a camera 
-cap = cv2.VideoCapture(0) 
-  
-# loop runs if capturing has been initialized. 
-while 1:  
-  
-    # reads frames from a camera 
-    ret, img = cap.read()  
-  
-    # convert to gray scale of each frames 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-  
-    # Detects faces of different sizes in the input image 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
-  
-    for (x,y,w,h) in faces: 
-        # To draw a rectangle in a face  
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
-        roi_gray = gray[y:y+h, x:x+w] 
-        roi_color = img[y:y+h, x:x+w] 
-  
-        # Detects eyes of different sizes in the input image 
-        eyes = eye_cascade.detectMultiScale(roi_gray)  
-  
-        #To draw a rectangle in eyes 
-        for (ex,ey,ew,eh) in eyes: 
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2) 
-  
-    # Display an image in a window 
-    cv2.imshow('img',img) 
-  
-    # Wait for Esc key to stop 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: 
+import cv2
+
+# Importing Haarcascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Turning on the webcam
+video = cv2.VideoCapture(0)
+
+while True:
+
+    check, frame = video.read()  # Boolean, image being returned
+
+    faces = face_cascade.detectMultiScale(frame, 1.1, 3)  # Face coordinates are detected.
+
+    # Rectangle shape for getting face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face_only = frame[y:y + h, x:x + w]
+
+    eyes = eye_cascade.detectMultiScale(face_only)  # 1.05, 10
+
+    # Rectangle shape for getting eyes
+    for (a, b, c, d) in eyes:
+        cv2.rectangle(face_only, (a, b), (a + c, b + d), (0, 0, 255), 2)
+
+    key = cv2.waitKey(1)  # Waitkey(1) so that each millisecond a frame is captured.
+
+    cv2.imshow('img', frame)  # To show the video
+
+    if key == ord('q'):  # To exit programitcally
         break
-  
-# Close the window 
-cap.release() 
-  
-# De-allocate any associated memory usage 
+
+video.release()
 cv2.destroyAllWindows()
